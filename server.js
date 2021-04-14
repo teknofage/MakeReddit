b3b8adac7f5f0093cd92e562b16fe17f26511c0d
teknofage <teknofage@gmail.com>
2021-04-14 16:03:30 -0700

    posts/new page showing with form

---
+++ server.js
@@ -0,0 +1,41 @@
+global.__basedir = __dirname;
+
+// Require Libraries
+const express = require('express')
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const app = express();
+const port = 3000;
+
+const dotenv = require('dotenv/config');
+const path = require('path'); 
+
+const cors = require('cors');
+
+console.log('Hello Node.js project.');
+console.log(process.env.MY_SECRET);
+
+// App Setup
+
+// Middleware
+app.engine("handlebars", exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// view engine setup
+app.use(express.static('public'));
+
+app.use(cors());
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
