d251294124b63d598bcccd7d7b1314c89d3ad22f
teknofage <teknofage@gmail.com>
2021-04-20 18:19:54 -0700

    still not showing new or existing posts, removed excess routes from server.js, tried different combinations of post/posts in routes

--- models/post.js
+++ models/post.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
+  _id: { type: String, required: true },
   title: { type: String, required: true },
   url: { type: String, required: true },
   summary: { type: String, required: true }
