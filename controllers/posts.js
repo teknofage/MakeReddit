2576eec2f9b5d474bc01240f2192ee5b079d3600
teknofage <teknofage@gmail.com>
2021-05-09 20:25:38 -0700

    users can only make comments when logged in

--- controllers/posts.js
+++ controllers/posts.js
@@ -44,9 +44,7 @@ module.exports = (app) => {
     // SHOW
     app.get("/posts/:id", function (req, res) {
         var currentUser = req.user;
-        // LOOK UP THE POST
-
-        Post.findById(req.params.id).lean().populate({path:'comments', populate: {path: 'author'}}).populate('author')
+        Post.findById(req.params.id).populate('comments').lean()
             .then(post => {
                 res.render("posts-show", { post, currentUser });  
             })
@@ -67,16 +65,16 @@ module.exports = (app) => {
         })
     })
 
-      // SUBREDDIT
-    app.get("/n/:subreddit", function(req, res) {
+    // SUBREDDIT
+    app.get("/n/:subreddit", function (req, res) {
         var currentUser = req.user;
-        Post.find({ subreddit: req.params.subreddit }).lean().populate('author')
-        .then(posts => {
-        res.render("posts-index", { posts, currentUser });
-        })
-        .catch(err => {
-        console.log(err);
-        });
+        Post.find({ subreddit: req.params.subreddit }).lean()
+            .then(posts => {
+                res.render("posts-index", { posts, currentUser });
+            })
+            .catch(err => {
+                console.log(err);
+            });
     });
 
     // FETCH USER
